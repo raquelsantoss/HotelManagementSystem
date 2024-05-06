a0acf88005e899a3d9695b28d13f683539d4d7ec
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-05-06 20:57:07 -0300

    feat: Create entity room and its method

--- src/main/java/org/example/hotel/entities/Room.java
+++ src/main/java/org/example/hotel/entities/Room.java
@@ -1,4 +1,74 @@
 package org.example.hotel.entities;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+
+    private static final int TIME_CLEAN = 1500;
+
+    private final String name;
+    private boolean available;
+    private boolean Clean;
+    private Maid maid;
+    private final List<Guest> guests;
+
+    public Room(String name) {
+        this.name = name;
+        this.guests = new ArrayList<>();
+        this.available = true;
+        this.Clean = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGuest(Guest guest) {
+        guests.add(guest);
+    }
+
+    public void removeAllGuest() {
+        guests.clear();
+    }
+
+    public void setMaid(Maid maid) {
+        this.maid = maid;
+    }
+
+    public void toggleAvailable() {
+        available = !available;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void cleanRoom() {
+        Clean = false;
+        System.out.println(maid.getMaidName() + " pegou a chave do " + name + " com a recepcionista");
+        System.out.println(maid.getMaidName() + " está limpando o quarto " + name);
+        try {
+            maid.sleep(TIME_CLEAN);
+            System.out.println(maid.getMaidName() + " terminou de limpar e deixou a chave do " + name + " com a recepcionista");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Clean = true;
+    }
+
+    public void checkout(){
+        toggleAvailable();
+        removeAllGuest();
+    }
+
+    public boolean isClean(){
+        return Clean;
+    }
+
+    public List<Guest> getGuests() {
+        return guests;
+    }
+
 }
