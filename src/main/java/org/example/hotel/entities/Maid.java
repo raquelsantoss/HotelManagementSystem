cc8f669972c8845e854a9095e390cf6b1ce16e76
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-05-06 22:03:49 -0300

    Criando o modelo da Maid
    Disponibilizando os recursos necessários para criar uma Maid

--- src/main/java/org/example/hotel/entities/Maid.java
+++ src/main/java/org/example/hotel/entities/Maid.java
@@ -1,4 +1,18 @@
 package org.example.hotel.entities;
 
-public class Maid {
+public class Maid extends Thread {
+
+    private final String name;
+
+
+    public Maid(String name) {
+        this.name = name;
+    }
+
+    public void run() {}
+
+    public String getMaidName() {
+        return name;
+    }
+
 }
