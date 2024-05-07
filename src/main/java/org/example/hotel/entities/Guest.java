c1d0e9e87eed163e472bba061943f9a3062a8723
Raquel Santos <raquel.pri@hotmail.com>
2024-05-06 21:04:56 -0300

    feat: creating constants and variables

--- src/main/java/org/example/hotel/entities/Guest.java
+++ src/main/java/org/example/hotel/entities/Guest.java
@@ -1,4 +1,23 @@
 package org.example.hotel.entities;
 
-public class Guest {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guest extends Thread {
+    private static final int TIME_WALK = 2000;
+    private static final int TIME_WAIT = 20000;
+
+    private static final int MAX_FAMILY_SIZE = 8;
+
+    private final String name;
+    private final List<Room> rooms = new ArrayList<>();
+    private int tries;
+    private boolean running;
+
+    public Guest(String name) {
+        this.name = name;
+        this.running = true;
+        this.tries = 0;
+    }
+
 }
